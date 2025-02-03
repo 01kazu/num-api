cfd17ce5730a7403ccbad19f5a995040c3ec49e8
01kazu <davidjoshua026@gmail.com>
2025-02-03 11:03:57 +0100

    todo created

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+import json
+
+'''
+functions to create
+1. is_prime(number) - returns true or false
+2. is_perfect(number) - returns true or false
+3. properties(number) - returns a list of the following existing properties: ['armstrong', 'odd', even']
+4. digit_sum(number) - sum of each digit.
+5. fun_fact(number) - use http://numbersapi.com
+
+6. main_api() - if alphacharacters are in the characters, return a 400 Bad Request
+'''
+
