181354b9c5fa0741cce22428d3ce02b016eccdc3
01kazu <davidjoshua026@gmail.com>
2025-02-05 10:55:28 +0100

    fixed is_prime()

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def is_prime(number: int) -> bool:
     '''
     if number <= 1:
         return False
-    for i in range(2, int(sqrt(number))):
+    for i in range(2, int(sqrt(number)) + 1 ):
         if number % i == 0:
             return False
     return True
